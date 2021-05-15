59ae79dc37bbc4060ef901234ce2532e3877aa46
arkko <arkko002@gmail.com>
2021-05-15 21:09:23 +0200

    Use file-type to detect extension of uploaded file

--- src/multer-config.js
+++ src/multer-config.js
@@ -1,4 +1,5 @@
 const multer = require("multer");
+const fileType = require("file-type");
 
 module.exports = multerConfig = {
     storage: multer.memoryStorage(),
@@ -8,12 +9,22 @@ module.exports = multerConfig = {
             return cb(new Error("No file was uploaded"));
         }
 
-        // TODO Better file type detection
-        const zipped = file.mimetype.startsWith("application/")
+        const zipped = isArchive(file);
         if(zipped) {
             cb(null, true);
         } else {
             return cb("Provided file was wrong format (must be a compressed archive)");
         }
-    }
+		
+    },
+
+	isArchive: (file) => {
+		let ext = fileType.fromBuffer(file).ext;
+
+		// TODO possibly more supported extensions
+		let archiveExts = ["zip", "tar", "rar", "gz", "7z", "bz2"];
+
+		// indexOf will be -1 if ext is not found in array
+		return archiveExts.indexOf(ext) != -1;
+	}
 }
